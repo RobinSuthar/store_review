e20c37a657075d1ddba5ea6964fc0582384b6f12
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-06-23 17:46:59 -0600

    Added ShrotNames

--- app/api/review/route.ts
+++ app/api/review/route.ts
@@ -1,6 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+import {
+  uniqueNamesGenerator,
+  Config,
+  adjectives,
+  colors,
+  animals,
+} from "unique-names-generator";
+
 const db = new PrismaClient();
+
+const customConfig: Config = {
+  dictionaries: [adjectives, colors],
+  separator: "-",
+  length: 2,
+};
+
 export const GET = async () => {
   return NextResponse.json({
     message: "Hello",
@@ -8,17 +23,11 @@ export const GET = async () => {
 };
 
 export const POST = async (req: NextRequest) => {
-  const { a, b, c } = await req.json();
-  console.log(a);
-  console.log(b);
+  const storeId = 1;
+  const { Wine, Beer, Liquore, Staff , Question1, Question2} = await req.json();
+const shortName: string = uniqueNamesGenerator(customConfig);
 
-  console.log(c);
-
-  //   const data = db.review.create({
-  //     data:{
-
-  //     }
-  //   })
+console.log(Wine, Beer, Liquore, Staff , Question1, Question2)    console.log(shortName);
   return NextResponse.json({
     message: "YEllow",
   });
