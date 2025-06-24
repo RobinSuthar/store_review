6d29e92b4097d57be973775a3a95c114a7e4a562
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-06-23 18:02:38 -0600

    Added Routing Setup Based On Cookies

--- app/api/review/route.ts
+++ app/api/review/route.ts
@@ -1,14 +1,15 @@
+import { cookies } from "next/headers";
+
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 import {
   uniqueNamesGenerator,
   Config,
   adjectives,
   colors,
-  animals,
 } from "unique-names-generator";
 
-const db = new PrismaClient();
+// const db = new PrismaClient();
 
 const customConfig: Config = {
   dictionaries: [adjectives, colors],
@@ -24,10 +25,16 @@ export const GET = async () => {
 
 export const POST = async (req: NextRequest) => {
   const storeId = 1;
-  const { Wine, Beer, Liquore, Staff , Question1, Question2} = await req.json();
-const shortName: string = uniqueNamesGenerator(customConfig);
 
-console.log(Wine, Beer, Liquore, Staff , Question1, Question2)    console.log(shortName);
+  const { Wine, Beer, Liquore, Staff, Question1, Question2 } = await req.json();
+  const shortName: string = uniqueNamesGenerator(customConfig);
+
+  console.log(storeId);
+  console.log(Wine, Beer, Liquore, Staff, Question1, Question2);
+  console.log(shortName);
+
+  const cookieStore = await cookies();
+  cookieStore.set("review", "yes");
   return NextResponse.json({
     message: "YEllow",
   });
