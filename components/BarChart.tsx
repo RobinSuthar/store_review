81590a40081b2b0a097a05dd7d3e28a4a223f261
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-06-29 23:29:14 -0600

    Added Correct Graph

--- components/BarChart.tsx
+++ components/BarChart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { TrendingUp } from "lucide-react";
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import { Bar, BarChart, XAxis, YAxis } from "recharts";
 
 import {
   Card,
@@ -18,54 +18,67 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 
-export const description = "A bar chart";
-
-const chartData = [
-  { month: "January", desktop: 186 },
-  { month: "February", desktop: 305 },
-  { month: "March", desktop: 237 },
-  { month: "April", desktop: 73 },
-  { month: "May", desktop: 209 },
-  { month: "June", desktop: 214 },
-];
+export const description = "A horizontal bar chart";
 
 const chartConfig = {
   desktop: {
-    label: "Desktop",
+    label: "Rating",
     color: "var(--chart-1)",
   },
 } satisfies ChartConfig;
 
-export function ChartBarDefault() {
+type Props = {
+  category: string;
+  Ratings: {
+    oneStar: number;
+    twoStar: number;
+    threeStar: number;
+    fourStar: number;
+    fiveStar: number;
+  };
+};
+
+export function ChartBarHorizontal(data: Props) {
+  const chartData = [
+    { month: "5 Star", desktop: data.Ratings.fiveStar },
+    { month: "4 Star", desktop: data.Ratings.fourStar },
+    { month: "3 Star", desktop: data.Ratings.threeStar },
+    { month: "2 Star", desktop: data.Ratings.twoStar },
+    { month: "1 Star", desktop: data.Ratings.oneStar },
+  ];
+
   return (
-    <Card className="w-6/12">
+    <Card>
       <CardHeader>
-        <CardTitle>Bar Chart</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
+        <CardTitle>{data.category}</CardTitle>
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig}>
-          <BarChart accessibilityLayer data={chartData}>
-            <CartesianGrid vertical={false} />
-            <XAxis
+          <BarChart
+            accessibilityLayer
+            data={chartData}
+            layout="vertical"
+            margin={{
+              left: -5,
+            }}
+          >
+            <XAxis type="number" dataKey="desktop" hide />
+            <YAxis
               dataKey="month"
+              type="category"
               tickLine={false}
               tickMargin={10}
               axisLine={false}
-              tickFormatter={(value) => value.slice(0, 3)}
             />
             <ChartTooltip
               cursor={false}
               content={<ChartTooltipContent hideLabel />}
             />
-            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={8} />
+            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={5} />
           </BarChart>
         </ChartContainer>
       </CardContent>
       <CardFooter className="flex-col items-start gap-2 text-sm">
-        <div className="flex gap-2 leading-none font-medium">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
         <div className="text-muted-foreground leading-none">
           Showing total visitors for the last 6 months
         </div>
