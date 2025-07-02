289cc64afdef483fabdaf879859237c4b2551792
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-07-01 22:31:24 -0600

    Added Required Graph for insights

--- app/admin/insights/page.tsx
+++ app/admin/insights/page.tsx
@@ -1,3 +1,19 @@
+import { ChartAreaStep } from "@/components/LinearBar";
+import { ChartRadarDots } from "@/components/RadarChart";
+import { ChartRadialSimple } from "@/components/RadicalChart";
+import { ChartTooltipIndicatorLine } from "@/components/TooltiplinearBar";
+
 export default function Insights() {
-  return <div>somgifnmlskam</div>;
+  return (
+    <div>
+      <div className="grid  grid-cols-4 gap-10">
+        <ChartAreaStep />
+        <ChartTooltipIndicatorLine />
+      </div>
+      <div className="grid mt-12 grid-cols-4 gap-10">
+        <ChartRadialSimple />
+        <ChartRadarDots />
+      </div>
+    </div>
+  );
 }
