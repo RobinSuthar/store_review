6fa3d62aa211c213f353c63f60e79f768a116c05
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-07-05 13:35:04 -0600

    Installed Next Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 export default async function Home() {
-  return <div>Hello</div>;
+  const cookieStore = await cookies();
+  const reviewSubmitted = cookieStore.get("review");
+  console.log("Check For Review Submitted :  ", reviewSubmitted);
+  if (!cookieStore.get("review")) {
+    redirect("/review");
+  } else {
+    redirect("/reviewed");
+  }
 }
