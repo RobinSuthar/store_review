eb07a27145c318af09810d3daae9e27aa432e199
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-07-05 12:38:32 -0600

    Resturcting the entire projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 export default async function Home() {
-  const cookieStore = await cookies();
-  const reviewSubmitted = cookieStore.get("review");
-  console.log("Check For Review Submitted :  ", reviewSubmitted);
-  if (!cookieStore.get("review")) {
-    redirect("/review");
-  } else {
-    redirect("/reviewed");
-  }
+  console.log("Hello");
 }
--- app/reviewed/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <div className="text-5xl  mt-6 p-6 ">Thank You for the Review !</div>;
-}
