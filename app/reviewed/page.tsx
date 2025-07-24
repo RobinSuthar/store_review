8255399cc2cf418a0479acad9e6f97ad6ce8273c
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-07-23 23:23:27 -0600

    Logic and page for once review added

--- app/reviewed/page.tsx
+++ app/reviewed/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-  return <div>Hello</div>;
+  return (
+    <div className="grid grid-rows-2 mt-52">
+      <div className="flex justify-center text-2xl">
+        You have already Submitted a Review
+      </div>
+      <div className="flex justify-center mt-2 text-xl">Thank You.</div>
+    </div>
+  );
 }
